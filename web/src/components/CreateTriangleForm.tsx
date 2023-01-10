6bce27b4c4ee1d1fa14073f211ce9cdc712490cf
billums123 <wthest123@gmail.com>
2023-01-09 21:00:01 -0700

    set up basic triangle form structure

--- web/src/components/CreateTriangleForm.tsx
+++ web/src/components/CreateTriangleForm.tsx
@@ -1,9 +1,18 @@
-import { Box, Button } from "@mui/material";
-import "../stylesheets/main-container.css";
+import { Box, Button, TextField, Typography } from "@mui/material";
+import "../stylesheets/create-triangle-form.css";
 const CreateTriangleForm = () => {
   return (
-    <Box>
+    <Box className="triangle-form">
+      <Typography>
+        Please provide the lengths for the 3 sides of your triangle.
+      </Typography>
+      <Box className="text-fields">
+        <TextField label="Side A"></TextField>
+        <TextField label="Side B"></TextField>
+        <TextField label="Side C"></TextField>
+      </Box>
       <Button variant="contained">Create a Triangle</Button>
+      <Box className="image-display">Image</Box>
     </Box>
   );
 };
