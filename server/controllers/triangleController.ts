e26307da9100f31ec8890f185ffdf535a4f496ba
billums123 <wthest123@gmail.com>
2023-01-13 11:48:51 -0700

    'update triangle routing'

--- server/controllers/triangleController.ts
+++ server/controllers/triangleController.ts
@@ -6,6 +6,8 @@ dotenv.config();
 
 interface TriangleController {
   saveTriangle: RequestResponseNext;
+  getTriangles: RequestResponseNext;
+  deleteTriangle: RequestResponseNext;
 }
 
 const triangleController: TriangleController = {
@@ -22,6 +24,8 @@ const triangleController: TriangleController = {
       const { userId } = res.locals;
       const user_id = userId;
 
+      const converted_triangle_image = Buffer.from(triangle_image, "base64");
+
       const saveTriangleText =
         "INSERT INTO triangles (type_by_side, type_by_angle, angle_a, angle_b, angle_c, triangle_image, user_id) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id";
       const saveTriangleValues = [
@@ -30,11 +34,10 @@ const triangleController: TriangleController = {
         angle_a,
         angle_b,
         angle_c,
-        triangle_image,
+        converted_triangle_image,
         user_id,
       ];
       const response = await db.query(saveTriangleText, saveTriangleValues);
-      console.log("RESPONSE", response.rows[0].id);
       res.locals.triangleId = response.rows[0].id;
       return next();
     } catch (error) {
@@ -42,6 +45,69 @@ const triangleController: TriangleController = {
         log: `Error caught in triangleController.saveTriangle ${error}`,
         status: 409,
         message: "Triangle already exists!",
+      });
+    }
+  },
+
+  getTriangles: async (req, res, next) => {
+    try {
+      const { userId } = res.locals;
+      const user_id = userId;
+
+      const getTrianglesText = "SELECT * FROM triangles WHERE user_id = $1";
+      const getTrianglesValues = [user_id];
+      const response = await db.query(getTrianglesText, getTrianglesValues);
+
+      if (response.rows[0].triangle_image !== "default") {
+        response.rows.forEach((row) => {
+          const buffer = Buffer.from(row.triangle_image);
+          const base_64 = buffer.toString("base64");
+          row.triangle_image = `data:image/png;base64,${base_64}`;
+        });
+      }
+      res.locals.triangles = response.rows;
+      return next();
+    } catch (error) {
+      return next({
+        log: `Error caught in triangleController.getTriangles ${error}`,
+        status: 404,
+        message: "Can't find triangles!",
+      });
+    }
+  },
+
+  deleteTriangle: async (req, res, next) => {
+    const triangleId = req.params.triangleId;
+
+    const verifyUserIdText = "SELECT user_id FROM triangles WHERE id = $1";
+    const verifyUserIdValues = [triangleId];
+    const verifyUserIdResponse = await db.query(
+      verifyUserIdText,
+      verifyUserIdValues
+    );
+    if (verifyUserIdResponse.rows[0].user_id !== res.locals.userId) {
+      return next({
+        log: null,
+        status: 403,
+        message: "You cannot delete accounts that are not your own",
+      });
+    }
+    try {
+      // delete the triangle with corresponding id number
+      const deleteTriangleText =
+        "DELETE FROM triangles where id = $1 returning id";
+      const deleteTriangleValues = [triangleId];
+      const deleteTriangleResponse = await db.query(
+        deleteTriangleText,
+        deleteTriangleValues
+      );
+      res.locals.triangleId = deleteTriangleResponse.rows[0].id;
+      return next();
+    } catch (error) {
+      return next({
+        log: `Error caught in triangleController.deleteTriangle ${error}`,
+        status: 404,
+        message: `Error has occured in triangleController.deleteTriangle ERROR: ${error}`,
       });
     }
   },
--- server/routes/triangle.ts
+++ server/routes/triangle.ts
@@ -4,13 +4,32 @@ import triangleController from "../controllers/triangleController";
 
 const router = express.Router();
 
-// create a new user and assign jwt
+// save new triangles
 router.post(
   "/",
   userController.verifyJWT,
   triangleController.saveTriangle,
   (req, res) => {
     res.status(200).json({ triangleId: res.locals.triangleId });
+  }
+);
+// read triangles
+router.get(
+  "/",
+  userController.verifyJWT,
+  triangleController.getTriangles,
+  (req, res) => {
+    res.status(200).json(res.locals.triangles);
+  }
+);
+
+// delete specific triangle
+router.delete(
+  "/:triangleId",
+  userController.verifyJWT,
+  triangleController.deleteTriangle,
+  (req, res) => {
+    res.status(204).json({ triangleId: res.locals.triangleId });
   }
 );
 
